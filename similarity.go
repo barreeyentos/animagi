51a8d1e2b0b356ab735e8699bc0e22639aa4d5b1
barreeyentos <ale.barrientos@gmail.com>
2017-12-09 13:48:22 -0800

    unoptimized depth checks

--- similarity.go
+++ similarity.go
@@ -2,7 +2,6 @@ package animagi
 
 import (
 	"errors"
-	"fmt"
 	"strings"
 )
 
@@ -35,15 +34,64 @@ func SimilarityRank(str1, str2 string) (rank uint) {
 		return MaxRank
 	}
 
-	str1Len := len(str1)
-	str2Len := len(str2)
-	shorterLen := str1Len
-
 	str1Depths := strings.Split(str1, ".")
 	str2Depths := strings.Split(str2, ".")
 
-	fmt.Println("str1 split", len(str1Depths), "-", str1Depths)
-	fmt.Println("str2 split", len(str2Depths), "-", str2Depths)
+	mostSimilarMatch := stringSimilarityRank(str1Depths[len(str1Depths)-1], str2Depths[len(str2Depths)-1])
+
+	var longerPath, shorterPath []string
+
+	if len(str1Depths) > len(str2Depths) {
+		longerPath = str1Depths
+		shorterPath = str2Depths
+	} else {
+		longerPath = str2Depths
+		shorterPath = str1Depths
+	}
+
+	differenceInPaths := len(longerPath) - len(shorterPath)
+
+	mostSimilarMatch += uint(dFactor * differenceInPaths)
+	mostSimilarMatch += mostSimilarSubPaths(append(longerPath[0:len(longerPath)-1]), append(shorterPath[0:len(shorterPath)-1]), differenceInPaths)
+
+	return mostSimilarMatch
+}
+
+func mostSimilarSubPaths(longerPath, shorterPath []string, differenceInPaths int) (rank uint) {
+	rank = MaxRank
+
+	if len(shorterPath) == 0 {
+		return 0
+	}
+
+	if differenceInPaths == 0 {
+		rank = 0
+		for i := 0; i < len(longerPath); i++ {
+			rank += stringSimilarityRank(longerPath[i], shorterPath[i])
+		}
+	} else {
+		for i := 0; i < len(longerPath); i++ {
+			adjusted := make([]string, len(longerPath))
+			copy(adjusted, longerPath)
+			subRank := mostSimilarSubPaths(append(adjusted[:i], adjusted[i+1:]...), shorterPath, differenceInPaths-1)
+			adjusted = nil
+			if subRank < rank {
+				rank = subRank
+			}
+
+			if rank == uint(dFactor*(differenceInPaths-1)) {
+				break
+			}
+		}
+	}
+
+	return rank
+}
+
+func stringSimilarityRank(str1, str2 string) (rank uint) {
+	str1Len := len(str1)
+	str2Len := len(str2)
+	shorterLen := str1Len
 
 	if str1Len == 0 {
 		return uint(mlFactor * str2Len)
@@ -69,14 +117,14 @@ func SimilarityRank(str1, str2 string) (rank uint) {
 }
 
 func validateString(str string) (err error) {
-	if str == "." {
+	if str == "." || str == " " {
 		err = errors.New(invalidString)
 	}
 	strLen := len(str)
 	if strLen > 0 {
 		prevChar := str[0]
 		for i := 1; i < len(str); i++ {
-			if prevChar == '.' && prevChar == str[i] || str[i] == ' ' {
+			if (prevChar == '.' && prevChar == str[i]) || str[i] == ' ' {
 				return errors.New(invalidString)
 			}
 			prevChar = str[i]
--- similarity_test.go
+++ similarity_test.go
@@ -11,6 +11,7 @@ var _ = Describe("Animagi", func() {
 
 	mlFactor := 5
 	lFactor := 1
+	dFactor := 3
 
 	Context("single depth strings", func() {
 		It("Should handle 1st param as empty strings", func() {
@@ -56,14 +57,19 @@ var _ = Describe("Animagi", func() {
 		})
 	})
 
-	FContext("Invalid strings", func() {
+	Context("Invalid strings", func() {
 		It("Should return MaxRank for '.'", func() {
 			rank := animagi.SimilarityRank(".", "valid")
 			Expect(rank).To(BeNumerically("==", animagi.MaxRank))
 		})
 
-		It("Should return MaxRank for ' '", func() {
+		It("Should return MaxRank for ' ' as first string", func() {
 			rank := animagi.SimilarityRank(" ", "valid")
+			Expect(rank).To(BeNumerically("==", animagi.MaxRank))
+		})
+
+		It("Should return MaxRank for ' ' as second string", func() {
+			rank := animagi.SimilarityRank("valid", " ")
 			Expect(rank).To(BeNumerically("==", animagi.MaxRank))
 		})
 
@@ -71,9 +77,54 @@ var _ = Describe("Animagi", func() {
 			rank := animagi.SimilarityRank("something..invalid", "valid")
 			Expect(rank).To(BeNumerically("==", animagi.MaxRank))
 		})
+
+		It("Should return MaxRank for string with space", func() {
+			rank := animagi.SimilarityRank("foo. .baar", "valid")
+			Expect(rank).To(BeNumerically("==", animagi.MaxRank))
+		})
 	})
 
 	Context("Strings of same depth", func() {
+		It("Should return 0 for same strings", func() {
+			rank := animagi.SimilarityRank("com.barreeyentos.animagi", "com.barreeyentos.animagi")
+			Expect(rank).To(BeZero())
+		})
 
+		It("Should return 1 for strings off by one letter", func() {
+			rank := animagi.SimilarityRank("com.barreeyentos.animag1", "com.barreeyentos.animagi")
+			Expect(rank).To(BeNumerically("==", 1*lFactor))
+		})
+	})
+
+	Context("Strings with different depths", func() {
+		It("Should return dFactor*2 for same string off by 2 depths", func() {
+			rank := animagi.SimilarityRank("animagi", "com.barreeyentos.animagi")
+			Expect(rank).To(BeNumerically("==", 2*dFactor))
+		})
+
+		It("Should find smallest rank for same string off by 1 depths", func() {
+			rank := animagi.SimilarityRank("com.animagi", "com.barreeyentos.animagi")
+			Expect(rank).To(BeNumerically("==", 1*dFactor))
+		})
+
+		It("Should find smallest rank for same string off by 2 depths", func() {
+			rank := animagi.SimilarityRank("user.employer.manager.name", "user.name")
+			Expect(rank).To(BeNumerically("==", 2*dFactor))
+		})
+
+		It("Should find smallest rank for same string off by 3 depths", func() {
+			rank := animagi.SimilarityRank("user.employer.manager.details.name", "manager.name")
+			Expect(rank).To(BeNumerically("==", 3*dFactor))
+		})
+
+		It("Should find smallest rank for same string off by 3 depths and wrong letters", func() {
+			rank := animagi.SimilarityRank("user.employer.maneger.details.name", "manager.name")
+			Expect(rank).To(BeNumerically("==", 3*dFactor+1*lFactor))
+		})
+
+		It("Should find smallest rank for same string off by 3 depths and wrong letter and extra letter", func() {
+			rank := animagi.SimilarityRank("user.employer.manegers.details.name", "manager.name")
+			Expect(rank).To(BeNumerically("==", 3*dFactor+1*lFactor+1*mlFactor))
+		})
 	})
 })
